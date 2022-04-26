ca0c0e25a6ba2fa358bda967b037df6c55cad38c
“Rene <rene.diaz@ucb.edu.bo>
2022-04-25 22:23:22 -0400

    HISTORIA DE USUARIO 1: (user interface) Responder Ganaste

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,32 @@
-import sumar from "./sumador";
+import TorosyVacas from "./torosVacas"
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+//Forms
+const codigoForm = document.querySelector("#codigo-form");
+const intentoForm = document.querySelector("#intento-form");
 
-form.addEventListener("submit", (event) => {
+//Inputs
+const codigoInput = document.querySelector("#codigo");
+const intentoInput = document.querySelector("#intento");
+
+//Resultados
+const respuestaDiv = document.querySelector("#respuesta-div");
+
+let torosVacas = "";
+
+codigoForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const codigoSecreto = codigoInput.value;
+  torosVacas = new TorosyVacas();
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+intentoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const intento = intentoInput.value;
+  const respuesta = torosVacas.adivinar(intento);
+
+  let respuestaHTML = `<p> ${respuesta} </p>`;
+  respuestaDiv.innerHTML = respuestaHTML;
 });
