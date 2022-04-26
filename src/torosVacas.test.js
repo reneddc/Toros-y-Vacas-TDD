5c3f9ff6ada97ee425d0c2f93811c7250629c562
“Rene <rene.diaz@ucb.edu.bo>
2022-04-25 22:33:02 -0400

    HISTORIA DE USUARIO 1: Test 2 (test failed) Responder NO si no adivina

--- src/torosVacas.test.js
+++ src/torosVacas.test.js
@@ -3,9 +3,14 @@ import TorosyVacas from "./torosVacas"
 describe("Toros y Vacas", () => {
   let torosVacas = new TorosyVacas("");
 
-  it("Debería responder <Ganaste> si se adivina el código secreto", () => {
+  it("Debería responder <GANASTE> si se adivina el código secreto", () => {
     torosVacas.definirCodigoSecreto("3");
     expect(torosVacas.adivinar("3")).toEqual("GANASTE");
+  });
+
+  it("Debería responder <NO> si no se adivina el código secreto", () => {
+    torosVacas.definirCodigoSecreto("3");
+    expect(torosVacas.adivinar("4")).toEqual("NO");
   });
 });
 
